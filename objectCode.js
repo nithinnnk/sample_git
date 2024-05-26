9980e3bc033b3c3718a10b0e3d024b29c93d43e9
Nithin <nithinkoushik05@gmail.com>
2024-05-27 00:47:37 +0530

    my first JS code

---
+++ objectCode.js
@@ -0,0 +1,11 @@
+const profile = {
+    age: 24, //int
+    fullName: "Knight Rider",  //string
+    isMale: true,  //bool
+    isMarried: false, // bool
+    company: "TS",  //string
+    cgpa: 8.28  //double
+};
+
+console.log(profile["fullName"]);
+console.log(profile.fullName);
